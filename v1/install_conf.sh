4c7e2ec76aff3ef00ec5a1712b29d96efccfe358
root <root@bus.local>
2022-04-23 17:03:39 +0300

    add script2

--- v1/install_conf.sh
+++ v1/install_conf.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cp /asterisk/v1/asterisk/*  /etc/asterisk
-cp /asterisk/sounds/*  /var/lib/asterisk/sounds
+cp /asterisk/v1/sounds/*  /var/lib/asterisk/sounds
+service asterisk start
+sleep 10
+/usr/sbin/asterisk -rx "module load chan_sip.so"
