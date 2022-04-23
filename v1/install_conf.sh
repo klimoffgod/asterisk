0daecd6baa099124fe74510df607d07eeeb894fb
root <root@bus.local>
2022-04-24 00:45:03 +0300

    fix

--- v1/install_conf.sh
+++ v1/install_conf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aptitude install  -y sox screen
+aptitude install  -y sox screen dialog  apt-utils
 cp /asterisk/v1/asterisk/*  /etc/asterisk
 cp /asterisk/v1/sounds/*  /var/lib/asterisk/sounds
-screen service asterisk start
+screen -d -m  service asterisk start
