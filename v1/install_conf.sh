71b4aa344919fa50c042a4d717e2582ef0b84232
root <root@bus.local>
2022-04-23 17:25:07 +0300

    add script

--- v1/install_conf.sh
+++ v1/install_conf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aptitude install  -y sox
+aptitude install  -y sox screen
 cp /asterisk/v1/asterisk/*  /etc/asterisk
 cp /asterisk/v1/sounds/*  /var/lib/asterisk/sounds
-service asterisk start
+screen service asterisk start
