ba77f37bfcf69ca15cdb0913c06213ef66b77d36
root <root@bus.local>
2022-04-24 13:06:28 +0300

    fix

--- v1/install_conf.sh
+++ v1/install_conf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aptitude install  -y sox screen dialog  apt-utils
+aptitude install  -y sox screen dialog  apt-utils net-tools iputils-ping unixodbc unixodbc-dev libcppdb-odbc0 odbcinst git checkinstall cmake build-essential libssl1.1
 cp /asterisk/v1/asterisk/*  /etc/asterisk
 cp /asterisk/v1/sounds/*  /var/lib/asterisk/sounds
 screen -d -m  service asterisk start
