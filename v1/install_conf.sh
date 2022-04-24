91197185558e3275ca209cb19ea919f4817948b6
root <root@bus.local>
2022-04-24 15:30:45 +0300

    fix1

--- v1/install_conf.sh
+++ v1/install_conf.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-aptitude install  -y sox screen dialog  apt-utils net-tools iputils-ping unixodbc unixodbc-dev libcppdb-odbc0 odbcinst git checkinstall cmake build-essential libssl1.1 default-mysql-client
-aptitude install -N  asterisk-mysql
+aptitude install  -y sox screen dialog  apt-utils net-tools iputils-ping unixodbc unixodbc-dev libcppdb-odbc0 odbcinst git checkinstall cmake build-essential libssl1.1 default-mysql-client asterisk-mysql
 cp /asterisk/v1/asterisk/*  /etc/asterisk
 cp /asterisk/v1/sounds/*  /var/lib/asterisk/sounds
 sh /asterisk/v1/build_connector_mariadb.sh
