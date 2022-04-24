73c4c0106ce2176b78fcbc3701ead1ab9d38d053
root <root@bus.local>
2022-04-24 15:18:57 +0300

    fix1

--- v1/add_user_db_aster.sh
+++ v1/add_user_db_aster.sh
@@ -1,4 +1,4 @@
 mysql -uroot -h mariadb -paster123 -e "CREATE DATABASE asteriskcdrdb /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-mysql -uroot -h mariadb -paster123 -e "CREATE USER asterisk@% IDENTIFIED BY 'aster123';"
-mysql -uroot -h mariadb -paster123 -e "GRANT ALL PRIVILEGES ON asteriskcdrdb.* TO 'asterisk'@'%';"
+mysql -uroot -h mariadb -paster123 -e "CREATE USER asterisk@0.0.0.0 IDENTIFIED BY 'aster123';"
+mysql -uroot -h mariadb -paster123 -e "GRANT ALL PRIVILEGES ON asteriskcdrdb.* TO 'asterisk'@'0.0.0.0';"
 mysql -uroot -h mariadb -paster123 -e "FLUSH PRIVILEGES;"
